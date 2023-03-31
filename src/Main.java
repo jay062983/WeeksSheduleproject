eedd7e9f74441991ef5dc83e3f7cf7aef92cb415
jay062983 <jaycs0617@gmail.com>
2023-03-31 16:56:04 -0400

    weeklymeals has been setup

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,11 @@ public class Main {
         schedule.BreakfastTime=8;
         schedule.mealsoftheday="protienshake";
 
+        weeklymeals wm=new weeklymeals();
+        wm.time=8;
+        wm.qty=22;
+        wm.type="healthy";
+        
+
     }
 }
